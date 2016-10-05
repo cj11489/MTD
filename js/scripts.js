7d45b43205c600045744ef6342df2fcfdfc3b931
Chris Joiner <cjoiner20@hotmail.co.uk>
2016-10-05 20:39:08 +0100

    fixed map issue. Optimized images. Added more media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,3 @@
-/*google map using js API*/
-function initMap() {
-  var mylatlng = {lat: 52.489471, lng: -1.898575};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: mylatlng,
-    zoom: 10
-  });
-
-  var marker = new google.maps.Marker({
-  	position: mylatlng,
-  	map: map,
-  	animation: google.maps.Animation.DROP,
-  	title: "Birmingham"
-  });
-
-  marker.setMap(map);
-}
-
 <!--Tooltips jQuery start here-->
 function () {
 	('#date').tooltip();
